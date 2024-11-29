58d2ef068cf4a502f173b93b15f99dc39a6033fa
senaerden58 <132837240+senaerden58@users.noreply.github.com>
2024-11-29 23:45:30 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuaforWebSitesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/scroll.js
@@ -0,0 +1,21 @@
+﻿const homeBody = document.querySelectorAll('.homeBody');
+
+const options = {
+    threshold: 0.3
+};
+
+const callBack = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callBack, options);
+
+homeBody.forEach((target) => {
+    observer.observe(target);
+});
